7d103744b8cb8635d19b435721c4bbe3e5c168e6
Gabriel Lima <gabriellima0526@gmail.com>
2018-11-27 21:40:47 -0200

    infonote_04

---
+++ infonote_04/src/controller/InfoNote.java
@@ -0,0 +1,17 @@
+package controller;
+
+import model.Usuario;
+
+public class InfoNote {
+	
+	boolean logado = false;
+		
+		Usuario user = new Usuario();
+		
+		public void mostrarMenu() {
+			System.out.println("======================================");
+		}
+		
+	}
+
+
