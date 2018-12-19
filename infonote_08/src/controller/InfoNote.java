d5ce899d6be3dfbe2b944ab30b46248b2ef99704
Gabriel Lima <gabriellima0526@gmail.com>
2018-12-19 18:41:04 -0200

    infonote_08

--- infonote_08/src/controller/InfoNote.java
+++ infonote_08/src/controller/InfoNote.java
@@ -21,19 +21,19 @@ public class InfoNote {
 	boolean logado = false;
 	
 		public InfoNote() {
-			notebooks[0] = new Notebook(1, "Negativo N22BR", "CPU Intel Core 2 Duo, Memória 2 GB, HD 250 GB",
+			notebooks[0] = new Notebook("1", "Negativo N22BR", "CPU Intel Core 2 Duo, Memória 2 GB, HD 250 GB",
 					6, 1200.00, "img\\\\n22br.jpg","19/05/2011");
 			
-			notebooks[1] = new Notebook(2, "Bell B55BR", "CPU Intel I3, Memória 4 GB, HD 500 GB",
+			notebooks[1] = new Notebook("2", "Bell B55BR", "CPU Intel I3, Memória 4 GB, HD 500 GB",
 					3, 1800.00, "img\\b55br.jpg", "20/05/2011");
 			
-			notebooks[2] = new Notebook(3, "Pompaq P41BR", "CPU Intel I3, Memória 3 GB, HD 320 GB",
+			notebooks[2] = new Notebook("3", "Pompaq P41BR", "CPU Intel I3, Memória 3 GB, HD 320 GB",
 					1, 1600.00, "img\\p41br.jpg", "21/05/2011");
 			
-			notebooks[3] = new Notebook(4, "CCF CR71CH","CPU Intel Dual Core, Memória 2 GB, HD 160 GB",
+			notebooks[3] = new Notebook("4", "CCF CR71CH","CPU Intel Dual Core, Memória 2 GB, HD 160 GB",
 					5, 1100.00, "img\\cr71ch.jpg", "10/06/2011");
 			
-			notebooks[4] = new Notebook(5, "BradescoTech BD22BR", "CPU AMD Phenon II, Memória 4 GB, HD 500 GB",
+			notebooks[4] = new Notebook("5", "BradescoTech BD22BR", "CPU AMD Phenon II, Memória 4 GB, HD 500 GB",
 					2, 1900.00, "img\\bd22br.jpg","10/06/2011");		
 		}
 		
@@ -174,7 +174,6 @@ public class InfoNote {
 			System.out.println("manterCarrinho - Em Construção");
 		}
 		
-		@SuppressWarnings("unlikely-arg-type")
 		public void inserirNotebook() {
 			//Lê o notebook escolhido do teclado
 			String numeroNote = Teclado.lerTexto("Informe o número do notebook" + 
